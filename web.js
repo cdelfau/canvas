d0017c4f7bd207e75a1307040f0220644b59a6e1
cdelfau <cdelfau@stuy.edu>
2017-02-14 00:56:49 -0500

    canvas hw

--- web.js
+++ web.js
@@ -1,4 +1,26 @@
+//creating the canvas
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
-ctx.fillstyle = "#ff0000";
-ctx.fillRect(50, 50, 100, 200);
+//ctx.fillstyle = "#ff0000";
+//ctx.fillRect(50, 50, 100, 200);
+
+
+//clearing the canvas
+var clearCanvas = function() {
+    ctx.clearRect(0,0, c.width, c.height);
+};
+var clear = document.getElementById("clearbutton");
+clear.addEventListener("click", clearCanvas);
+
+var drawRect = function(d) {
+    var mouseX = d.offsetX;
+    var mouseY = d.offsetY;
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(mouseX-10,mouseY-10,20,20);
+};
+c.addEventListener("click", drawRect);
+//
+
+
+
+
