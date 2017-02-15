70ea1ea2e5e9b41eeb930f1817be336fd08362a7
cdelfau <cdelfau@stuy.edu>
2017-02-14 21:06:14 -0500

    made the dots and the lines sort of work so far but not entirely

--- web.js
+++ web.js
@@ -6,19 +6,40 @@ var clrbutton = document.getElementById("clr");
 //clearing the canvas
 var clearCanvas = function() {
     ctx.clearRect(0,0, c.width, c.height);
+    ctx.closePath()
+    begin = true;
 };
 
 //drawing a rectangle
-var drawRect = function(d) {
-    var mouseX = d.offsetX;
-    var mouseY = d.offsetY;
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(mouseX-10, mouseY-10, 30, 30);
-};
+//var drawRect = function(d) {
+//    var x = d.offsetX;
+//    var y = d.offsetY;
+//    ctx.fillStyle = "#000000";
+//    ctx.fillRect(x-10, y-10, 10, 10);
+//};
+
+//connecting the dots
+var connect = function(f) {
+    var begin = true;
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineWidth= "1px";
+    var x = f.offsetX;
+    var y = f.offsetY;
+    if (begin) {
+	ctx.moveTo(x, y)
+	begin = false;
+    }  
+    ctx.arc(x, y, 5, 0, 2*Math.PI);
+    ctx.lineTo(x, y);
+    ctx.stroke();
+};    
 
 //test calls
-c.addEventListener("click", drawRect);
+//c.addEventListener("click", drawRect);
+c.addEventListener('click', connect);
 clrbutton.addEventListener("click", clearCanvas);
+
 
 
 
