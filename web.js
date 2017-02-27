9328c814798fec758e58299b20bce85e9919c9cb
cdelfau <cdelfau@stuy.edu>
2017-02-27 13:37:55 -0500

    Update web.js

--- web.js
+++ web.js
@@ -85,6 +85,44 @@ var stopIt = function() {
     window.cancelAnimationFrame(requestID);
 };
 
+lastX = -1;
+lastY = -1;
+
+var drawPath = function(e) {
+    var mouseX = e.offsetX;
+    var mouseY = e.offsetY;
+    if (lastX == -1 && lastY == -1) {
+	lastX = mouseX;
+	lastY = mouseY;
+    } else {
+	ctx.strokeStyle = "rgb("+
+  		Math.floor(Math.random()*256)+","+
+  		Math.floor(Math.random()*256)+","+
+  		Math.floor(Math.random()*256)+")";
+	ctx.beginPath();
+	ctx.moveTo(lastX,lastY);
+	ctx.lineTo(mouseX,mouseY);
+	ctx.stroke();
+	ctx.fillStyle = "rgb("+
+  		Math.floor(Math.random()*256)+","+
+  		Math.floor(Math.random()*256)+","+
+  		Math.floor(Math.random()*256)+")";
+	ctx.beginPath();
+	ctx.moveTo(lastX,lastY);
+	ctx.arc(lastX,lastY,10,0,Math.PI*2);
+	ctx.stroke();
+	ctx.fill();
+	lastX = mouseX;
+	lastY = mouseY;
+    };
+};
+
+var clr = document.getElementById("clr");
+clr.addEventListener("click",clearCanvas);
+
+c.addEventListener("click",drawPath);
+//c.addEventListener("click",drawSquare);
+
 //testcalls
 //c.addEventListener("click", drawRect);
 button.addEventListener("click", clear);
